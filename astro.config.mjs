7a5880c6e8a103c02c44a164236303aad8a35999
M4rteaga <m4rteagag@gmail.com>
2022-08-25 22:49:56 -0500

    core: add syntax highlighting with shink

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), mdx()]
+	markdown: {
+		shikiConfig: {
+			theme: 'github-dark',
+			wrap: true,
+		},
+	},
+	integrations: [solidJs(), mdx()],
 });
