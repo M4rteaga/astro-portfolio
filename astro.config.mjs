e4d440d237c41959dd5e0d043b3801e48f60668b
M4rteaga <m4rteagag@gmail.com>
2022-09-03 14:24:10 -0500

    feat: enable plugins to add custom classes to mdx/md files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
-
+import addClasses from './add-classes.mjs';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
@@ -10,6 +10,28 @@ export default defineConfig({
 			theme: 'github-dark',
 			wrap: true,
 		},
+		rehypePlugins: [
+			[addClasses, { a: 'md-ancher' }],
+			[addClasses, { 'h2,h3,h4,h5,h6': 'md-headings' }],
+			[addClasses, { img: 'md-img' }],
+			[addClasses, { p: 'md-p' }],
+			[addClasses, { h1: 'md-h1' }],
+			[addClasses, { li: 'md-li' }],
+			[addClasses, { article: 'article' }],
+		],
 	},
-	integrations: [solidJs(), mdx()],
+	integrations: [
+		solidJs(),
+		mdx({
+			rehypePlugins: [
+				[addClasses, { a: 'md-ancher' }],
+				[addClasses, { 'h2,h3,h4,h5,h6': 'md-headings' }],
+				[addClasses, { img: 'md-img' }],
+				[addClasses, { p: 'md-p' }],
+				[addClasses, { h1: 'md-h1' }],
+				[addClasses, { li: 'md-li' }],
+				[addClasses, { article: 'article' }],
+			],
+		}),
+	],
 });
